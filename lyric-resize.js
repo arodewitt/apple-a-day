8caa342f68ebac7ade45700eacc0339863dc996e
arodewitt <alicia@Alicias-MacBook-Pro.local>
2017-11-26 16:21:08 -0500

    initial commit

---
+++ lyric-resize.js
@@ -0,0 +1,5 @@
+var resizeText = document.getElementsByClassName('lyric-snip');
+var h = document.getElementsByClassName('lyric-container').offsetHeight;
+var x = h / 10;
+
+resizeText[0].style.fontSize = x;
