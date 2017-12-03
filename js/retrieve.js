c236a55f954a2d732c6f12fbdb4c478275bbbcc3
alicia dewitt <aro.dewitt@gmail.com>
2017-12-03 15:27:16 -0500

    adds randomization functionality on load, adds comments to js

--- js/retrieve.js
+++ js/retrieve.js
@@ -1,4 +1,5 @@
 var container = document.getElementById('lyric_container');
+// gets json array
 var requestURL = '/data/song-info.json';
 var request = new XMLHttpRequest();
 
@@ -11,16 +12,24 @@ request.onload = function() {
   populateText(songLyric);
 }
 
+// uses populateText to write content from json
 function populateText(jsonObj) {
   var myH1 = document.createElement('h1');
   var myH2a = document.createElement('h2');
   var myH2b = document.createElement('h2');
   var myH2c = document.createElement('h2');
 
-  myH1.textContent = jsonObj.songs[0].quote;
-  myH2a.textContent = jsonObj.songs[0].song;
-  myH2b.textContent = jsonObj.songs[0].album;
-  myH2c.textContent = jsonObj.songs[0].year;
+// gets max-value for random array entry
+  var l = jsonObj.songs.length;
+// creates variable based on randomly selected array entry
+  var r = Math.floor(Math.random() * (l + 1));
+  console.log(r);
+
+// prints content of randomly selected entry
+  myH1.textContent = jsonObj.songs[r].quote;
+  myH2a.textContent = jsonObj.songs[r].song;
+  myH2b.textContent = jsonObj.songs[r].album;
+  myH2c.textContent = jsonObj.songs[r].year;
 
   container.appendChild(myH1);
   container.appendChild(myH2a);
